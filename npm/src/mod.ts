2ae25f085b1ec041c2f45581145cc942dffa3018
GunseiKPaseri <GunseiKPaseri@gmail.com>
2022-10-29 01:22:36 +0900

    fix

--- npm/src/mod.ts
+++
@@ -1,3 +0,0 @@
-// Copyright 2022 GunseiKPaseri All rights reserved. MIT license.
-export { PromiseArray } from './src/promise_array_parallel.js'
-export type { ParallelWorkOptions } from './src/promise_array_parallel.js'
